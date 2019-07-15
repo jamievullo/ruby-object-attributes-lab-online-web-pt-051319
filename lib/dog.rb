8b36d77c7be6169382d467bee2d47fcf8fc61377
Jamie Vullo <jvullo@protonmail.com>
2019-07-15 13:18:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,18 @@
 class Dog 
   
-  # def initialize(name)
-  #   @name = new_name
-  #   @breed = new_breed
-  # end
+  def name=(name)
+    @name = name
+  end
   
   def name
     @name
   end
   
-  def name=(new_name)
-    @name = new_name
+  def breed=(breed)
+    @breed = breed
   end
   
   def breed 
-    @breed
-  end
-  
-  def breed=(new_breed)
-    @breed = new_breed
-  end
-  
+    @breed 
+  end 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,19 @@
 class Person
   
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job)
+    @job = job
+  end
+  
   def job
     @job
   end
   
-  def job=(new_job)
-    @job = new_job
-  end
-  
-  def name 
-    @name
-  end
-  
-  def name=(new_name)
-    @name = new_name
-  end
 end
-    
